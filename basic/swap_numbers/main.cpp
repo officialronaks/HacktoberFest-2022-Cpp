422b0f4e91090823cf240fc669835d9e151c6984
Sanyam <sanyam.makhija17@gmail.com>
2022-10-09 18:51:17 +0530

    Update main.cpp
    The input numbers get swap in this program.

--- basic/swap_numbers/main.cpp
+++ basic/swap_numbers/main.cpp
@@ -4,7 +4,10 @@ using namespace std;
  
 void swap(int &a,int &b){
    //Enter your code here
-}
+   int temp = a;
+   a=b;
+   b=temp;
+}      //Swaped the numbers with call by reference.
 
 //Driver Code
 int main()
