0894d65f947b933f4e1bd14eabeeb609f6e7e67b
Hardik <hardika1503@gmail.com>
2022-10-12 23:44:23 +0530

    Bubble Sort Recursive Solution

--- sorting/bubble_sort_recursion/main.cpp
+++ sorting/bubble_sort_recursion/main.cpp
@@ -8,7 +8,7 @@ void bubbleSort_recursive(int arr[],int n)
     if (n == 1)
         return;
  
-    for (int i=0; i<n-1; i++)
+    for (int i=0; i<=n-2; i++)
         if (arr[i] > arr[i+1])
             swap(arr[i], arr[i+1]);
  
