3dfe5082aa17d56869f67c1560e2ca6c5a3fe334
shaina45 <shaina4598@gmail.com>
2022-10-14 14:21:58 +0530

    This is the solution of insertion sort using recursion

--- sorting/insertion_sort_recursion/main.cpp
+++ sorting/insertion_sort_recursion/main.cpp
@@ -6,6 +6,21 @@ using namespace std;
 void insertionSort_recursion(int arr[],int size)
 {  
          // enter your code here
+    if (n <= 1)
+        return;
+ 
+    insertionSortRecursive( arr, n-1 );
+ 
+    int last = arr[n-1];
+    int j = n-2;
+ 
+
+    while (j >= 0 && arr[j] > last)
+    {
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = last;
 }
 
 
