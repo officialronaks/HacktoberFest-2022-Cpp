ecbda0f34d0d03b660be06dc61ec66ad726783a0
Veeno Rai <101185711+Veeno-Rai@users.noreply.github.com>
2022-10-14 13:27:28 +0530

    Merge_Sort_Recursively
    This program contain function to sort an array using merge sort algorithm. The time complexity of merge sort algorithm is  O(nlogn).

--- sorting/merge_sort_recursion/main.cpp
+++ sorting/merge_sort_recursion/main.cpp
@@ -1,11 +1,57 @@
 // C++ program to implement Merge Sort using Recursion
 #include <iostream>
 using namespace std;
- 
+// function to merge  sorted array
+void  merge(int arr[],int l,int mid,int r)
+{
+    int n1=mid-l+1;
+    int n2=r-mid;
+    int a[n1],b[n2];
+    for(int i=0;i<n1;i++)
+    {
+        a[i]=arr[i+l];
+    }
+    for(int i=0;i<n2;i++)
+    {
+        b[i]=arr[mid+i+1];
+    }
+    int i=0;
+    int j=0;
+    int k=l;
+    while(i<n1&&j<n2)
+    {
+    if(a[i]<b[j])
+    {
+        arr[k]=a[i];
+        k++;i++;
+    }
+    else{
+        arr[k]=b[j];
+        k++;j++;
+    }
+    }
+    while(i<n1)
+    {
+        arr[k]=a[i];
+        i++;k++;
+    }
+    while(j<n2)
+    {
+        arr[k]=b[j];
+        j++;k++;
+    }
+}
 
-void mergeSort_recursive(int arr[],int begin,int end)
+
+void mergeSort_recursive(int arr[],int l,int r)
 {  
-         // enter your code here
+         int mid=(l+r)/2;
+         if(l<r)
+           {
+              merge_sort(arr,l,mid);
+              merge_sort(arr,mid+1,r);
+              merge(arr,l,mid,r);
+            }
 }
 
 
