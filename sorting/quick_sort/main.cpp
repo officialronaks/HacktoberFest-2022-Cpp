7981c971bbf3e26037375e425c683f39cd801b06
Saksham Sharma <89659787+SakshamSharma07@users.noreply.github.com>
2022-10-14 13:22:02 +0530

    quick_sort
    This program sort the given array using quick sort

--- sorting/quick_sort/main.cpp
+++ sorting/quick_sort/main.cpp
@@ -1,34 +1,69 @@
 // C++ program to implement Quick Sort
 #include <iostream>
 using namespace std;
- 
 
-void quickSort(int arr[],int begin,int end)
-{  
-         // enter your code here
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-
-void printArray(int arr[], int n)  // function to print elements of array
+int partition(int arr[], int low, int high)
 {
-    for (int i=0; i < n; i++)
+    int pivot = arr[high]; // pivot
+    int i = (low - 1);     // Index of smaller element and indicates
+                           // the right position of pivot found so far
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int n) // function to print elements of array
+{
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
- 
-//Driver Code
+
+// Driver Code
 int main()
-{   
+{
     int size;
-    cin>>size; //size of array
+    cin >> size; // size of array
     int arr[size];
-    
-    for(int i=0;i<size;i++){
-       cin>>arr[i]; // elemnents in the array
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i]; // elemnents in the array
     }
-   
-   quickSort(arr,0,size-1);
-   
-   printArray(arr, size);
-   return 0;
+
+    quickSort(arr, 0, size - 1);
+
+    printArray(arr, size);
+    return 0;
 }
