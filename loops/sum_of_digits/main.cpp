92d0aefab3c78521a52629beb0409e1e8c28987d
Shantanu Rajput <shantanurajput520@gmail.com>
2022-10-15 11:25:05 +0530

    Exception cases added

--- loops/sum_of_digits/main.cpp
+++ loops/sum_of_digits/main.cpp
@@ -3,14 +3,22 @@ using namespace std;
  
 double sum_of_digits(int n){
    //Enter your code here
+   
    int sum=0,temp;
-   while(n>0)
+   if(n>0)
    {
-        temp=n%10;    
-        sum=sum+temp;    
-        n=n/10;
+        while(n>0)
+        {
+            temp=n%10;    
+            sum=sum+temp;    
+            n=n/10;
+        }
+        return sum;
    }
-   return sum;
+   else
+   {
+       return -1;
+   }
 }
 
 //Driver Code
