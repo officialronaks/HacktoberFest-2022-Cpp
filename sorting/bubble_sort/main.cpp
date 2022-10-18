1f314e8b613bd23476586c55fd85ae2a8393781b
pratikkumar009 <115658704+pratikkumar009@users.noreply.github.com>
2022-10-18 20:36:35 +0530

    Update main.cpp

--- sorting/bubble_sort/main.cpp
+++ sorting/bubble_sort/main.cpp
@@ -6,6 +6,21 @@ using namespace std;
 void bubbleSort(int arr[],int size)
 {  
          // enter your code here
+  int i, j;
+	 for (i = 0; i < size; ++i)
+	 {
+		 for (j = 0; j < size-i-1; ++j)
+		 {
+			 // Comparing consecutive data and switching values if value at j > j+1.
+			 if (arr[j] > arr[j+1])
+			 {
+				 arr[j] = arr[j]+arr[j+1];
+				 arr[j+1] = arr[j]-arr[j + 1];
+				 arr[j] = arr[j]-arr[j + 1];
+			 }
+		 }
+		// Value at n-i-1 will be maximum of all the values below this index.
+	 }	
 }
 
 
