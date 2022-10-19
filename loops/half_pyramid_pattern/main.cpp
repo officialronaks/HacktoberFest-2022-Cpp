de3d37d27c24c0309fa73b089467d7e9c488c50b
ranjan_4590 <ranjangupta35558@gmail.com>
2022-10-19 11:40:46 +0530

    task done

--- loops/half_pyramid_pattern/main.cpp
+++ loops/half_pyramid_pattern/main.cpp
@@ -3,6 +3,12 @@ using namespace std;
  
 void half_pyramid_pattern(int n){
    //Enter your code here
+   for(int i=0;i<n;i++){
+    for(int j=0;j<i+1;j++){
+        cout<<"*"<<" ";
+    }
+    cout<<endl;
+   }
 }
 int main()
 {   
